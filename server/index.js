b6a496923f991d0eba5c51058dd371075b693890
unknown <miguel.valarao@gmail.com>
2021-05-21 14:27:52 -0700

    Add stripe route

--- server/index.js
+++ server/index.js
@@ -14,5 +14,36 @@ app.get("/", (req, res) => {
     res.send("It works!");
 });
 
+app.post("/payment", async (req, res) => {
+    try {
+        const { product, token } = req.body;
+        console.log("PRODUCT ", product);
+        console.log("PRICE ", product.price);
+        const idempontencyKey = uuid();
+        const customer = await stripe.customers.create({
+            email: token.email,
+            source: token.id
+        });
+
+        const stripeResult = await stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'cad',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `purchase of ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        }, { idempontencyKey });
+
+        return res.send(200).json(stripeResult);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 // listen
 app.listen(8282, () => console.log("listening at port 8282"));
